55b52b0c9cf30ab0f38ccec493383f8982075a00
Vladan Vachalec <vladan.vachalec@hp.com>
2012-04-26 13:41:59 +0200

    requirement #5: another new branch commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -3,7 +3,7 @@ package com.mycompany.app;
 /**
  * Hello world
  * comment 2
- * commensassat daasd3 dsaasa
+ * commensasssat daasd3 dsaasa
  * reqwe\dxssdsdssddaaadsdsdsdssassadvvasausdaewasdasasirement 1
  * requiremsent 1 ssss   
  *
