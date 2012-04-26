115e5c648359d42388ebc2d4e214a375cc1dce4f
Vladan Vachalec <vladan.vachalec@hp.com>
2012-04-26 15:13:17 +0200

    1 commit to MASTER

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,7 +4,7 @@ package com.mycompany.app;
  * Hello world
  * comment 2a
  * commensasssat daasds3 dsaasa
- * reqwe\dxssdsdssddaaadsdsasaassasdsdssassadvvasausdaewasdasasirement 1
+ * reqwe\dxssdsdssddaaadsdsasaassasddsdssassadvvasausdaewasdasasirement 1
  * requiremsent 1 ssss   
  *
  */
